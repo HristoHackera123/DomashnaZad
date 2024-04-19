8753cdbdff33472a5ae9b51352dcf89a54292359
HristoHackera123 <hrisi.belezirev@gmail.com>
2024-04-19 07:52:47 +0300

    Stable ver

---
+++ DomashnaSZad/Employers.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomashnaSZad
+{
+    public class Employers
+    {
+        private string name;
+        private double salary;
+        private string position;
+        private string field;
+        private string? email;
+        private int? age;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                while (value == "" )
+                {
+                    Console.WriteLine("Name is mandatory");
+                    value = Console.ReadLine();
+                }
+                name = value;
+            }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set
+            { 
+                while (value == 0d)
+                {
+                    Console.WriteLine("Salary is mandatory");
+                    try
+                    {
+                        value = double.Parse(Console.ReadLine());
+                    }
+                    catch { value = 0d; }
+                }
+                salary = value;
+            }
+        }
+        public string Position
+        {
+            get { return position; }
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Position is mandatory");
+                    value = Console.ReadLine();
+                }
+                position = value;
+            }
+        }
+        public string Field
+        {
+            get { return field; }
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Field is mandatory");
+                    value = Console.ReadLine();
+                }
+                field = value;
+            }
+        }
+        public string? Email
+        {
+            get { return email; }
+            set
+            {
+                if (value != "")
+                {
+                    email = value;
+                }
+                else
+                {
+                    email = "n/a";
+                }
+            }
+        }
+        public int? Age
+        {
+            get { return age; }
+            set
+            {
+                if (value != 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    age = -1;
+                }
+            }
+        }
+        public Employers(string name, double salary, string position, string field, string? email, int? age)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Field = field;
+            Email = email;
+            Age = age;
+        }
+        public Employers()
+        {
+        }
+
+    }
+}
---
+++ DomashnaSZad/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DomashnaSZad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employers> list = new List<Employers>();
+            Console.Write("How much epmployees? ");
+            int N = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Input info for employee {i+1} ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = 0;
+                try
+                {
+                    salary = double.Parse(Console.ReadLine());
+                }
+                catch {}
+                Console.Write("Pos: ");
+                string position = Console.ReadLine();
+                Console.Write("Field: ");
+                string field = Console.ReadLine();
+                Console.Write("Email: ");
+                string? email = Console.ReadLine();
+                Console.Write("Age: ");
+                int? age = 0;
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+                }
+                catch {}
+                Employers employee = new Employers(name, salary, position, field, email, age);
+                list.Add(employee);
+            }
+
+            Employers highest = new Employers();
+            highest.Salary = double.MinValue;
+            for (int i = 0;i < list.Count;i++)
+            {
+                if (list[i].Salary > highest.Salary)
+                {
+                    highest.Salary = list[i].Salary;
+                    highest.Field = list[i].Field;
+                }
+            }
+            Console.WriteLine($"Highest average salary is in {highest.Field}");
+            for (int i =0;i<list.Count;i++)
+            {
+                if (list[i].Field == highest.Field)
+                {
+                    Console.WriteLine(string.Join(" ", list[i].Name, list[i].Salary, list[i].Email, list[i].Age));
+                }
+            }
+        }
+    }
+}
